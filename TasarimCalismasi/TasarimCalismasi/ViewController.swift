8b324f4573fb828c33460a4e39056c71ec0fb690
yasemintk3 <yasemin.tok0702@gmail.com>
2024-03-12 16:02:04 +0300

    constraint And StackView

--- TasarimCalismasi/TasarimCalismasi/ViewController.swift
+++ TasarimCalismasi/TasarimCalismasi/ViewController.swift
@@ -11,7 +11,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Pizza"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "anaRenk")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "yaziRenk1")!,
+                                          .font: UIFont(name: "Whisper-Regular", size: 25)!]
+        navigationController?.navigationBar.barStyle = .black //temam koyu saati ve diğerlerini açık yap
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        
     }
 
 
