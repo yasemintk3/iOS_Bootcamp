5d8d7e5a0bd3876bc9dba422e0bb566bcc5e4e93
yasemintk3 <yasemin.tok0702@gmail.com>
2024-03-17 13:34:13 +0300

    tabBar

--- TabBarKullanimi/TabBarKullanimi/ViewController.swift
+++ TabBarKullanimi/TabBarKullanimi/ViewController.swift
@@ -11,9 +11,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let tabItems = tabBarController?.tabBar.items {
+            let anasayfaItem = tabItems[0]
+            let ayarlarItem = tabItems[1]
+            
+            anasayfaItem.badgeValue = "3"
+            ayarlarItem.badgeValue = "Yeni"
+        }
+        
+        let appearance = UITabBarAppearance() // arkaplan rengi değiştirme
+        appearance.backgroundColor = UIColor.systemIndigo
+        
+        renkDegistir(itemAppearance: appearance.stackedLayoutAppearance)
+        renkDegistir(itemAppearance: appearance.inlineLayoutAppearance)
+        renkDegistir(itemAppearance: appearance.compactInlineLayoutAppearance)
+        
+        tabBarController?.tabBar.standardAppearance = appearance
+        tabBarController?.tabBar.scrollEdgeAppearance = appearance
     }
-
-
+    
+    func renkDegistir(itemAppearance:UITabBarItemAppearance) {
+        //seçili durum
+        
+        itemAppearance.selected.iconColor = UIColor.systemOrange
+        itemAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor.systemOrange]
+        itemAppearance.selected.badgeBackgroundColor = UIColor.systemMint
+        
+        //seçili olmayan durum
+        
+        itemAppearance.normal.iconColor = UIColor.white
+        itemAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor.white]
+        itemAppearance.normal.badgeBackgroundColor = UIColor.lightGray
+        
+    }
 }
 
